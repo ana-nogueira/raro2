8084b9d77f0029784b44d3d6f5b93d9c7142a24b
Ana Nogueira <acsnogueira95@gmail.com>
2025-04-27 14:42:38 -0300

    ajustando a barra de contato flutuante

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-const mobileNav = document.querySelector ('.mobile-nav-wrapper');
-const burgerMenuBtn = document.querySelector ('.burger-menu-btn');
-const closeBurgerMenu = document.querySelector ('.close-menu-btn')
+const mobileNav = document.querySelector('.mobile-nav-wrapper');
+const burgerMenuBtn = document.querySelector('.burger-menu-btn');
+const closeBurgerMenu = document.querySelector('.close-menu-btn');
 
-
-function openMobileMenu () {
-    mobileNav.classList.add('show');
+function openMobileMenu() {
+  mobileNav.classList.add('show');
 }
 
-function closeMobileMenu () {
-    mobileNav.classList.remove ('show');
+function closeMobileMenu() {
+  mobileNav.classList.remove('show');
 }
 
 closeBurgerMenu.addEventListener('click', closeMobileMenu);
 
-burgerMenuBtn.addEventListener ('click', openMobileMenu);
-
+burgerMenuBtn.addEventListener('click', openMobileMenu);
 
 /* CONTACT WINDDOW */
 
+const contactWindowBtn = document.querySelector('.contact-window .btn');
 
+function changeBtn(seta, corClasse) {
+  const textBtn = contactWindowBtn.querySelector('.btnText');
+  const imgBtn = contactWindowBtn.querySelector('img');
+  imgBtn.setAttribute('src', `./assets/img/${seta}.svg`);
+
+  textBtn.classList.remove('cor-5', 'cor-7');
+  textBtn.classList.add(corClasse);
+}
+
+contactWindowBtn.addEventListener('mouseover', () =>
+  changeBtn('seta', 'cor-7'),
+);
+contactWindowBtn.addEventListener('mouseleave', () =>
+  changeBtn('setablue', 'cor-5'),
+);
